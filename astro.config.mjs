6154f981c3ba21f3c4b5aa4f76b005f5d18598ed
Mic Harris <micharris@MicnoMacBook-Pro.local>
2026-06-18 10:25:54 +1000

    Replace sitemap plugin with static sitemap.xml in public/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   site: 'https://www.robwilliamsbjj.com',
 });
